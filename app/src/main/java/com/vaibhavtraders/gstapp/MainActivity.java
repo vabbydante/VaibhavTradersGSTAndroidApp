372debd629246009c00dbddc90e09a4cd5bbef10
vabbydante <rohitvaibhavgupta@gmail.com>
2023-11-04 13:54:54 +0530

    Added Fragments and Navigation Drawer Completely Working.

--- app/src/main/java/com/vaibhavtraders/gstapp/MainActivity.java
+++ app/src/main/java/com/vaibhavtraders/gstapp/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -34,25 +37,35 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 int id = item.getItemId();
+                item.setChecked(true);
                 drawerLayout.closeDrawer(GravityCompat.START);
                 if (id == R.id.nav_home) {
                     Toast.makeText(MainActivity.this, "Home is clicked.", Toast.LENGTH_SHORT).show();
+                    replaceFragment(new HomeFragment());
                 } else if (id == R.id.nav_profile) {
                     Toast.makeText(MainActivity.this, "Profile is clicked.", Toast.LENGTH_SHORT).show();
+                    replaceFragment(new ProfileFragment());
                 } else if (id == R.id.nav_settings) {
                     Toast.makeText(MainActivity.this, "Settings is clicked.", Toast.LENGTH_SHORT).show();
+                    replaceFragment(new SettingsFragment());
                 } else if (id == R.id.nav_help) {
                     Toast.makeText(MainActivity.this, "Help is clicked.", Toast.LENGTH_SHORT).show();
+                    replaceFragment(new HelpFragment());
                 } else if (id == R.id.nav_countries) {
                     Toast.makeText(MainActivity.this, "Countries is clicked.", Toast.LENGTH_SHORT).show();
+                    replaceFragment(new CountriesFragment());
                 } else if (id == R.id.nav_customers) {
                     Toast.makeText(MainActivity.this, "Customers is clicked.", Toast.LENGTH_SHORT).show();
+                    replaceFragment(new CustomersFragment());
                 } else if (id == R.id.nav_deliverymodes) {
                     Toast.makeText(MainActivity.this, "Delivery Modes is clicked.", Toast.LENGTH_SHORT).show();
+                    replaceFragment(new DeliveryModesFragment());
                 } else if (id == R.id.nav_products) {
                     Toast.makeText(MainActivity.this, "Products is clicked.", Toast.LENGTH_SHORT).show();
+                    replaceFragment(new ProductsFragment());
                 } else if (id == R.id.nav_states) {
                     Toast.makeText(MainActivity.this, "States is clicked.", Toast.LENGTH_SHORT).show();
+                    replaceFragment(new StatesFragment());
                 } else {
                     return true;
                 }
@@ -60,5 +73,12 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
     }
 }
