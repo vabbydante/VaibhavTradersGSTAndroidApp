84f96a0e780b20e400d96fa06313187c07883242
vabbydante <rohitvaibhavgupta@gmail.com>
2023-11-12 08:25:42 +0530

    Added Customer Form

---
+++ app/src/main/java/com/vaibhavtraders/gstapp/CustomerForm.java
@@ -0,0 +1,14 @@
+package com.vaibhavtraders.gstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CustomerForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_form);
+    }
+}
